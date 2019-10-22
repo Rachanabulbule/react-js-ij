634aa9b0d06366e261396020a607205634037921
Rachanab <rachana.bulbule@knoldus.in>
2019-10-22 15:25:52 +0530

    CURD application with ceprate components

--- src/App.js
+++ src/App.js
@@ -1,96 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Wishes from "./components/Greetings"
+import CURD from "./components/CURD"
 
-class App extends React.Component {
-
-  constructor(props){
-    super(props);
-    this.state={
-      title: "CURD application using ReactJs",
-      action: 0,
-      index: "",
-      data: []
-    }
-  } 
-
-  componentDidMount(){
-    this.refs.name.focus();
-  }
-
-  functionSubmit = (e) =>{
-    e.preventDefault();
-    console.log('try');
-
-    var data = this.state.data;
-    var name = this.refs.name.value;
-    var address = this.refs.address.value;
-
-    if(this.state.action === 0){   
-      var dataHold = {
-        name, address
-      }
-      data.push(dataHold);
-    }else{                      
-      var index = this.state.index;
-      data[index].name = name;
-      data[index].address = address;
-    }    
-
-    this.setState({
-      data: data,
-      act: 0
-    });
-
-    this.refs.curdForm.reset();
-    this.refs.name.focus();
-  }
-
-  functionRemove = (i) => {
-    var data = this.state.data;
-    data.splice(i,1);
-    this.setState({
-      data: data
-    });
-
-    this.refs.curdForm.reset();
-    this.refs.name.focus();
-  }
-
-  functionEdit = (i) => {
-    var dataHold = this.state.data[i];
-    this.refs.name.value = dataHold.name;
-    this.refs.address.value = dataHold.address;
-
-    this.setState({
-      action: 1,
-      index: i
-    });
-
-    this.refs.name.focus();
-  }  
-
-  render() {
-    var data = this.state.data;
-    return (
-      <div className="App">
-        <h2>{this.state.title}</h2>
-        <form ref="curdForm" className="curdForm">
-          <input type="text" ref="name" placeholder="your Good name" className="formField" />
-          <input type="text" ref="address" placeholder="your address" className="formField" />
-          <button onClick={(e)=>this.functionSubmit(e)} className="myButton">submit </button>
-        </form>
-        <pre>
-          {data.map((dataHold, i) =>
-            <li key={i} className="myList">
-              {i+1}. {dataHold.name}, {dataHold.address}
-              <button onClick={()=>this.functionRemove(i)} className="myListButton">remove </button>
-              <button onClick={()=>this.functionEdit(i)} className="myListButton">edit </button>
-            </li>
-          )}
-        </pre>
-      </div>
+class App extends React.Component{
+render(){
+return(
+  <div className= "App">
+      <Wishes/>
+      <CURD/>
+  </div>
     )
-  }
-}
-
+   }
+ }
 export default App
--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,19 +1,21 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function wishes(){
-  const date = new date()
+class Greetings extends React.Component{
+render(){
+  const date = new Date()
   const hours = date.getHours()
   const styles = { fontSize: 30}
-  var timeOfDay 
+  var timeOfDay
   if(hours<12){timeOfDay = "Morning"}
   else if(hours>=12&&hours<17){
       timeOfDay = "Afternoon"
   }
   else{timeOfDay = "Evening"}
 
-return(<main>
+return(<header className="Greeting">
     <h1 style={styles}>Good {timeOfDay}</h1>
-    </main>
+    </header>
   )
 }
-export default wishes
+}
+export default Greetings
